66b53d2ad407285d95da2bea0746d125064a8744
LuizPauloJRM <luizpaulomedeiros77@hotmail.com>
2025-11-09 21:31:15 -0300

    Explicando um objeto

---
+++ getStarted/src/getStarted/Main.java
@@ -0,0 +1,25 @@
+package getStarted;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Método pricipal 
+		//Tudo executavel deve estar dentro desse método 
+		//Objeto algo que representa o mundo real 
+		//Caneta um objeto que contem seus atributos
+		//Caracteristica e estado , comportamentos 
+		//Modelo para criar objetos 
+		//Caneta : Objeto 
+		//Molde : Classe 
+		//Classe : Coisas eu tenho , coisas eu faço , como estou 
+		//Métodos: Escrever , rabiscar , pintar , tampar , destampar 
+		//Atributos : Modelo , cor , ponta , tampada , carga 
+		//Estado : 
+		//Para criar um objeto tenho que definir uma classe , planejamento 
+		//Classe -> Caneta -> Definir atributos caracteristicas  
+		//Método rabiscar()
+		//Todo método pode receber parametros 
+
+	}
+
+}
