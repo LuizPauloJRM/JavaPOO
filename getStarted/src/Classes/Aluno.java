6efe32a9e557273788d8141d599c7e4378526879
LuizPauloJRM <luizpaulomedeiros77@hotmail.com>
2026-02-12 22:22:54 -0300

    Classes e construtores , trabalhando objetos , instanciando objetos
    Exemplo Classe aluno  criando varios alunos e fazendo contrutores para receber um nome e idade padrao quando eles são chamados

---
+++ getStarted/src/Classes/Aluno.java
@@ -0,0 +1,35 @@
+package Classes;
+
+public class Aluno {
+	//Classe do tipo Aluno 
+	//Classe/ Objeto que representa aluno com seus atributos 
+	//São atributos de um aluno 
+	String nome; 
+	int idade; 
+	float nota;
+	String dataNascimento;
+	String cpf;
+	int matricula;
+	
+	//Construtor : Cria os dados na memória
+	//Exemplo o new chama um construtor 
+	//Controi esse objeto na memoria 
+	
+	public Aluno() {
+		
+	}
+	public Aluno (String nomePadrao) {
+		//nome sempre sera o nome padrao do parâmetro
+		//Passamos o Valor padrao
+		nome= nomePadrao;
+		
+	}
+	
+	//Outro Construtor
+	public Aluno(String nomePadrao , int idadePadrao) {
+		nome = nomePadrao; 
+		idade= idadePadrao;
+	}
+	
+	
+}
---
+++ getStarted/src/executavel/Main.java
@@ -0,0 +1,28 @@
+package executavel;
+
+//Quando não esta no mesmo pacote preciso importar essa Classe Aluno 
+import Classes.Aluno;
+
+public class Main {
+	//Método executável do Java
+	public static void main(String[]args) {
+		
+		
+		//Criando objetos
+		//Iniciando aluno1
+		Aluno aluno1=new Aluno();//Exemplo Joao
+		
+		//Objeto ainda não existe na memória 
+		//Aluno aluno1;
+		
+		//Instanciando , criando um aluno da classe Aluno 	
+		Aluno aluno2= new Aluno();//Luiz
+		//Objeto do tipo Aluno do tipo da classe 
+		
+		//Nome padrao com construtor 
+		Aluno aluno3 = new Aluno("Luiz");
+		
+		//Nome e idade 
+		Aluno aluno4 = new Aluno ("Paulo",45);
+	}
+}
