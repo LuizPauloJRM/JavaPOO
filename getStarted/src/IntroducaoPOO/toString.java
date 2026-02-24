97227003f2a45123dba19ff0aa2dfefe071b05d8
LuizPauloJRM <luizpaulomedeiros77@hotmail.com>
2026-02-23 22:11:57 -0300

    toString

---
+++ getStarted/src/IntroducaoPOO/toString.java
@@ -0,0 +1,10 @@
+package IntroducaoPOO;
+
+public class toString {
+	public static void main(String[] args) {
+		
+		Carro passat = new Carro();
+		System.out.println(passat.toString());//Descricao na memória 
+		
+	}
+}
